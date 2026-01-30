da6074a7de4e0b2dead13ba9544e300be5d4345c
MyeishaSermonia <mbhg9348@leeds.ac.uk>
2026-01-30 10:02:52 +0000

    W1Sesh2_Progress

--- session_2/1_aggregators/tasks.sql
+++ session_2/1_aggregators/tasks.sql
@@ -7,3 +7,13 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+
+-- SELECT continent, AVG(Population) FROM countries GROUP BY continent;
+
+-- SELECT continent, SUM(Population) FROM countries GROUP BY continent;
+
+-- SELECT continent, country, MAX(Population) AS Population FROM countries GROUP BY continent;
+
+-- SELECT continent, country, MIN(AreaSqMi) AS LandArea FROM countries GROUP BY continent;
+
+-- SELECT continent, COUNT(*) AS TotalCountries FROM countries GROUP BY continent
